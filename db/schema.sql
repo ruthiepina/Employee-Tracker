855d6e68d029f69dc2f2ef1da4aabfa286aa8b88
RMP <ruthiepina@gmail.com>
2024-01-21 08:53:32 -0700

    Fixed id, role_id, department_id in class declarations. Created schema and seeds for required tables, also fixing id's within those.

--- db/schema.sql
+++ db/schema.sql
@@ -7,23 +7,39 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-department_id INT NOT NULL PRIMARY KEY,
+id INT NOT NULL PRIMARY KEY,
 name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-role_id INT NOT NULL PRIMARY KEY,
+id INT NOT NULL PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 department_id INT,
+CONSTRAINT fk_department
+FOREIGN KEY (department_id)
+REFERENCES departments (id)
+ON DELETE SET NULL,
 salary DECIMAL(8,2) NOT NULL    --DECIMAL(p,s) p is tot num of digits in value, s is scale so dig after decimal point.--
 );
 
 CREATE TABLE employees (
-employee_id INT NOT NULL PRIMARY KEY,
+id INT NOT NULL PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-title VARCHAR(30) NOT NULL,
-department_id INT,
+role_id INT NOT NULL,
+CONSTRAINT fk_role
+FOREIGN KEY (role_id)
+REFERENCES roles (id)
+ON DELETE SET NULL,
+department_id INT NOT NULL,
+CONSTRAINT fk_department
+FOREIGN KEY (department_id)
+REFERENCES departments (id)
+ON DELETE SET NULL,
 salary DECIMAL(8,2) NOT NULL,
-manager_id INT
-)
+manager_id INT DEFAULT NULL,
+CONSTRAINT fk_manager
+FOREIGN KEY (manager_id)
+REFERENCES employees (id)
+ON DELETE SET NULL
+);
